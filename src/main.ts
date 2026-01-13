5a5c804aa5f340802fa7f7187a062db110414caa
JoaoVictorNobuntu <joaovictor@nobuntu.com.br>
2026-01-13 05:56:54 -0300

    implement AppController and AppService with getHello method; update main.ts for CORS logging

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
   app.enableCors({ origin: process.env.CORS_ORIGIN });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
