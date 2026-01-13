a4e91abcab99a72599407799d14defb9635f8f8d
JoaoVictorNobuntu <joaovictor@nobuntu.com.br>
2026-01-13 06:41:12 -0300

    Fix: cors options

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ require('dotenv').config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
-  app.enableCors({ origin: process.env.CORS_ORIGIN });
+  app.enableCors({ origin: {
+  origin: [
+    process.env.CORS_ORIGIN,
+    'http://localhost:4200'                  // Para testes locais
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+} });
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
