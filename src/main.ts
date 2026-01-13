f2ee63b060802fbf85a30ceaa05788677ff8f251
JoaoVictorNobuntu <joaovictor@nobuntu.com.br>
2026-01-13 06:49:51 -0300

    Test: test on cloud cors enable all

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,9 @@ require('dotenv').config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
-  app.enableCors({
-  origin: [
-    'https://seu-front-angular.onrender.com', // Sem barra no final
-    'http://localhost:4200'                  // Para testes locais
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+app.enableCors({
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
 });
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
