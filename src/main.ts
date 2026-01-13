b39330b4a9864dbef33b9305d00b484b16f98ee6
JoaoVictorNobuntu <joaovictor@nobuntu.com.br>
2026-01-13 06:52:27 -0300

    test: cors enable all

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ require('dotenv').config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('CORS_ORIGIN:', process.env.CORS_ORIGIN);
-app.enableCors({
-  origin: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-});
+// O CORS deve ser uma das primeiras coisas!
+  app.enableCors({
+    origin: '*', // Teste com '*' para garantir que o problema é aqui
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
